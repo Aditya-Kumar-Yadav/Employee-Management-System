30633d000fc380edcda9080c9270e596ff3982a0
Aditya-Kumar-Yadav <advyadav16@gmail.com>
2019-06-28 18:02:56 +0530

    Compeleted the delete section

--- Employee_Mangement_System/src/Employee/Delete.java
+++ Employee_Mangement_System/src/Employee/Delete.java
@@ -51,49 +51,77 @@ class Delete
         showDeletedRecordBtn = new JButton("Show All Deleted Records");
         showDeletedRecordBtn.setBounds(180, 185, 220, 20);
         
+        	//2 pairs of DISPLAY is going to be created so we need all 2 tables COLUMN_NAME.
+        		//DISPLAY 1 & 3
         String[] tableColumn = {"EmpId", "FirstName", "LastName", "Father", "Gender", "Nationality", "Birth-Date", "Mobie No.", "Qualification"};
+        		//DISPLAY 2 & 4
         String[] addressTableColumn = {"Department", "Join-Date", "Address-Id", "Room-No", "Area", "City", "Pincode", "State"};
-        	//DELETE_EMPLOYEE_TABLE
+        	
+        	//FIRST_DISPLAY
+        		//DEFAULT_TABLE_MODEL create to make a TABLE_FORMATTED_DISPLAY
         DefaultTableModel tableModel = new DefaultTableModel();
-        for(String columnName : tableColumn)
-        	tableModel.addColumn(columnName);
+        		//all COLUMN_NAMES are set by using SET_COLUMN_IDENTIFIERS_METHOD
+        tableModel.setColumnIdentifiers(tableColumn);
+        		//now we INITILIZE our display with TABLE_FORMATTED_DISPLAY
         deleteEmployeeTable = new JTable(tableModel);
+        		//getting the COLUMN_MODEL to make changes in column structure
         TableColumnModel columnModel = deleteEmployeeTable.getColumnModel();
         columnModel.getColumn(0).setMaxWidth(50);
         columnModel.getColumn(3).setMaxWidth(60);
         columnModel.getColumn(4).setMaxWidth(50);
         columnModel.getColumn(6).setMaxWidth(60);
         columnModel.getColumn(7).setMaxWidth(70);
-        JScrollPane deletedEmployee = new JScrollPane(deleteEmployeeTable);
-        deletedEmployee.setBounds(10, 80, 570, 40);
+        		//we need JSCROLLPANE if data is more than the TABLE_SIZE
+    				//so JSCROLLPANE is created and INTIALIZED with our DISPLAY
+        JScrollPane deleteEmployee = new JScrollPane(deleteEmployeeTable);
+        		//SETBOUNDS is used to set the CO-ORDINATES and SIZE of DISPLAY
+        deleteEmployee.setBounds(10, 80, 570, 40);
         
+        	//SECOND_DISPLAY
+        		//DEFAULT_TABLE_MODEL create to make a TABLE_FORMATTED_DISPLAY
         DefaultTableModel addressTableModel = new DefaultTableModel();
-        for(String columnName : addressTableColumn)
-        	addressTableModel.addColumn(columnName);
+        		//all COLUMN_NAMES are set by using SET_COLUMN_IDENTIFIERS_METHOD
+        addressTableModel.setColumnIdentifiers(addressTableColumn);
+        		//now we INITILIZE our display with TABLE_FORMATTED_DISPLAY
         deleteEmployeeAddressTable = new JTable(addressTableModel);
-        JScrollPane deletedEmployeeAddress = new JScrollPane(deleteEmployeeAddressTable);
-        deletedEmployeeAddress.setBounds(10, 120, 570, 40);
+        		//we need JSCROLLPANE if data is more than the TABLE_SIZE
+    				//so JSCROLLPANE is created and INTIALIZED with our DISPLAY
+        JScrollPane deleteEmployeeAddress = new JScrollPane(deleteEmployeeAddressTable);
+        		//SETBOUNDS is used to set the CO-ORDINATES and SIZE of DISPLAY
+        deleteEmployeeAddress.setBounds(10, 120, 570, 40);
         
-        	//ALL_DELETED_EMPLOYEE_TABLE
+        	//THIRD_DISPLAY
+        		//DEFAULT_TABLE_MODEL create to make a TABLE_FORMATTED_DISPLAY
         DefaultTableModel allTableModel = new DefaultTableModel();
-        for(String columnName : tableColumn)
-        	allTableModel.addColumn(columnName);
+        		//all COLUMN_NAMES are set by using SET_COLUMN_IDENTIFIERS_METHOD
+        allTableModel.setColumnIdentifiers(tableColumn);
+        		//now we INITILIZE our display with TABLE_FORMATTED_DISPLAY
         allDeletedEmployeeTable = new JTable(allTableModel);
+        		//getting the COLUMN_MODEL to make changes in column structure
         TableColumnModel columnModelAll = allDeletedEmployeeTable.getColumnModel();
         columnModelAll.getColumn(0).setMaxWidth(50);
         columnModelAll.getColumn(3).setMaxWidth(60);
         columnModelAll.getColumn(4).setMaxWidth(50);
         columnModelAll.getColumn(6).setMaxWidth(60);
         columnModelAll.getColumn(7).setMaxWidth(70);
-        JScrollPane deleted = new JScrollPane(allDeletedEmployeeTable);
-        deleted.setBounds(10, 215, 570, 100);
+        		//we need JSCROLLPANE if data is more than the TABLE_SIZE
+    				//so JSCROLLPANE is created and INTIALIZED with our DISPLAY
+        JScrollPane allDeletedEmployee = new JScrollPane(allDeletedEmployeeTable);
+        		//SETBOUNDS is used to set the CO-ORDINATES and SIZE of DISPLAY
+        allDeletedEmployee.setBounds(10, 215, 570, 100);
         
+        	//FOURTH_DISPLAY
+        		//DEFAULT_TABLE_MODEL create to make a TABLE_FORMATTED_DISPLAY
         DefaultTableModel allAddressTableModel = new DefaultTableModel();
-        for(String columnName : addressTableColumn)
-        	allAddressTableModel.addColumn(columnName);
+        		//all COLUMN_NAMES are set by using SET_COLUMN_IDENTIFIERS_METHOD
+        allAddressTableModel.setColumnIdentifiers(addressTableColumn);
+        		//now we INITILIZE our display with TABLE_FORMATTED_DISPLAY
         allDeletedEmployeeAdressTable = new JTable(allAddressTableModel);
-        JScrollPane deleted1 = new JScrollPane(allDeletedEmployeeAdressTable);
-        deleted1.setBounds(10, 320, 570, 100);
+        		//we need JSCROLLPANE if data is more than the TABLE_SIZE
+    				//so JSCROLLPANE is created and INTIALIZED with our DISPLAY
+        JScrollPane allDeletedEmployeeAddress = new JScrollPane(allDeletedEmployeeAdressTable);
+        		//SETBOUNDS is used to set the CO-ORDINATES and SIZE of DISPLAY
+        allDeletedEmployeeAddress.setBounds(10, 320, 570, 100);
         
         	//ADDING ACTION_LISTENER to the BUTTONS
         searchBtn.addActionListener(new ActionListener()
@@ -136,17 +164,93 @@ class Delete
         					
         					tableModel.addRow(employeeTableData);
         					addressTableModel.addRow(employeeAddressTableData);
-        					
-        					
+        					JOptionPane.showMessageDialog(rightPanel, "Record Found");
         				}
         				catch(SQLException sqle)
         				{
-        					System.out.println(sqle);
+        					JOptionPane.showMessageDialog(rightPanel, "No Record Found");
         				}
         			}
         		});
-        //deleteBtn.addActionListener();
-        //showDeletedRecordBtn.addActionListener();
+        deleteBtn.addActionListener(new ActionListener() {
+		        	public void actionPerformed(ActionEvent e)
+		        	{
+		        		try
+		        		{
+		        			String emp_id = empIdTxt.getText();
+		        			if(tableModel.getRowCount() != 0)
+        					{
+        						tableModel.removeRow(0);
+        						addressTableModel.removeRow(0);
+        					}
+		        			if("".equals(emp_id))
+		        			{
+		        				JOptionPane.showMessageDialog(rightPanel, "Invalid Employee Id");
+		        			}
+		        			else
+		        			{
+				        		PreparedStatement copyDataSqlQuery = conn.prepareStatement("begin insert into deleted_employee_table select * from employee_table where emp_id = ?; insert into deleted_address_table select * from address_table where emp_id = ?; end;");
+				        			
+				        		copyDataSqlQuery.setString(1, emp_id);
+				        		copyDataSqlQuery.setString(2, emp_id);
+				        			
+				        		PreparedStatement deleteDataSqlQuery = conn.prepareStatement("begin delete from employee_table where emp_id = ?; delete from address_table where emp_id = ?; end;");
+				        			
+				        		deleteDataSqlQuery.setString(1, emp_id);
+				        		deleteDataSqlQuery.setString(2, emp_id);
+				        			
+				        		copyDataSqlQuery.executeUpdate();
+				        		deleteDataSqlQuery.executeUpdate();
+				        		empIdTxt.setText("");
+				        		JOptionPane.showMessageDialog(rightPanel, "Record Deleted");
+			        		}
+		        		}
+		        		catch(SQLException sqle)
+		        		{
+		        			JOptionPane.showMessageDialog(rightPanel, "No Record Found");
+		        		}
+		        	}
+        });
+        showDeletedRecordBtn.addActionListener(new ActionListener() {
+	        	public void actionPerformed(ActionEvent e)
+	        	{
+	        		try
+	        		{
+	        			PreparedStatement showDeletedRecord = conn.prepareStatement("select * from deleted_employee_table, deleted_address_table where (deleted_employee_table.emp_id = deleted_address_table.emp_id)");
+	        			ResultSet deletedRecordResult = showDeletedRecord.executeQuery();
+	        			
+	        			Object[] employeeTableData = new Object[9];
+    					Object[] employeeAddressTableData = new Object[9];
+    					int k, i, j;
+    					while(deletedRecordResult.next())
+    					{
+    						k = 1;
+    						i = 0;
+    						j = 0;
+    						while(k <= 17)
+    							{
+		    						if(i < 9)
+		    						{
+		    							employeeTableData[i] = deletedRecordResult.getString(k);
+		    						}
+		    						else if(j < 8)
+		    						{
+		    							employeeAddressTableData[j] = deletedRecordResult.getString(k);
+		    							j++;	
+		    						}
+		    						i++;
+		    						k++;
+    							}
+    						allTableModel.addRow(employeeTableData);
+    						allAddressTableModel.addRow(employeeAddressTableData);
+    					}
+	        		}
+	        		catch(SQLException sqle)
+	        		{
+	        			System.out.println(sqle);
+	        		}
+	        	}
+        });
         
             //Adding COMPONENTS to the DISPLAY_PANEL
         rightPanel.add(titleLbl);
@@ -154,14 +258,14 @@ class Delete
         
         rightPanel.add(empIdTxt);
         
-        rightPanel.add(deletedEmployee);
-        rightPanel.add(deletedEmployeeAddress);
+        rightPanel.add(deleteEmployee);
+        rightPanel.add(deleteEmployeeAddress);
         
         rightPanel.add(searchBtn);
         rightPanel.add(deleteBtn);
         rightPanel.add(showDeletedRecordBtn);
-        rightPanel.add(deleted);
-        rightPanel.add(deleted1);
+        rightPanel.add(allDeletedEmployee);
+        rightPanel.add(allDeletedEmployeeAddress);
         
             //RETURNING the completed DISPLAY_PANEL
         return rightPanel;
